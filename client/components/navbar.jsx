de19a7082d6a32cbaad4ec93a076ac396cfee353
Paul Glorioso <goldcansio@gmail.com>
2022-02-18 17:52:30 -0800

    Completed navbar with non-working links functional.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -6,22 +6,19 @@ export default function Navbar() {
       <i className="fas fa-bars" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample"></i>
       <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
         <div className="offcanvas-header">
-          <h5 className="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+          <h1 className="offcanvas-title" id="offcanvasExampleLabel">TRAILBLAZE</h1>
           <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
-          <div>
-            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+          <div className="nav-links">
+            <a href='#'><h2>Search Trails</h2></a>
+            <a href='#'><h2>Your Trails</h2></a>
+            <a href='#'><h2>Saved Trails</h2></a>
           </div>
-          <div className="dropdown mt-3">
-            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-              Dropdown button
-            </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+        </div>
+        <div className="offcanvas-footer">
+          <div className="nav-links">
+            <a href='#'><h2>Log-In/Create</h2></a>
           </div>
         </div>
       </div>
